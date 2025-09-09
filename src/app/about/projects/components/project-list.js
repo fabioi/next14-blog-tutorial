94cbd9a7fd4d3c3afc298b5432fecc229bb44bfb
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-09-09 11:28:58 +0200

    Refactor ProjectsPage to use ProjectList component for displaying repository data

---
+++ src/app/about/projects/components/project-list.js
@@ -0,0 +1,16 @@
+export default async function ProjectList() {
+      const response = await fetch("http://localhost:3001/repos", { cache: 'no-store' })
+      const repos = await response.json()
+
+      return (
+            <ul className="space-y-4">
+                  {repos.map((repo) => (
+                        <li key={repo.id} className="mb-4">
+                              <div>{repo.title}</div>
+                              <div>{repo.description}</div>
+                              <div>{repo.stargazers_count}</div>
+                        </li>
+                  ))}
+            </ul>
+      )
+}
