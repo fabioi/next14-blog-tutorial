3b79523f042ee207aa0b6f26a19d8322aa830b51
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-09-11 12:06:49 +0200

    Integrate react-error-boundary for error handling in ProjectsPage; update project description and wrap ProjectList in ErrorBoundary

--- src/app/about/projects/page.js
+++ src/app/about/projects/page.js
@@ -1,6 +1,7 @@
 import ProjectList from "@/app/about/projects/components/project-list"
 import ProjectListLoading from "@/app/about/projects/components/project-list-loading"
 import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 
 export default async function ProjectsPage() {
 
@@ -8,10 +9,13 @@ export default async function ProjectsPage() {
     <>
       <div>
         <h1 className="mb-8 text-xl">Projects</h1>
-        <div className="mb-8">Hello, this is the list of my projects</div>
-        <Suspense fallback={<ProjectListLoading />}>
-          <ProjectList />
-        </Suspense>
+        <div className="mb-8">Hello, this is the list of my repos!</div>
+        {/* Suspense wrapper to show loading component while ProjectList fetches data */}
+        <ErrorBoundary fallback={<div>Cannot fetch repos</div>}>
+          <Suspense fallback={<ProjectListLoading />}>
+            <ProjectList />
+          </Suspense>
+        </ErrorBoundary>
       </div>
 
     </>
