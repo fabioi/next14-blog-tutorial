625578df2b0eb39a254d211dcd765658e93ba9a3
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-09-11 11:50:19 +0200

    Add error handling in ProjectList component

--- src/app/about/projects/components/project-list.js
+++ src/app/about/projects/components/project-list.js
@@ -4,6 +4,8 @@ export default async function ProjectList() {
       const response = await fetch("http://localhost:3001/repos", { cache: 'no-store' })
       const repos = await response.json()
 
+      throw new Error("Error");
+
       return (
             <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   {repos.map((repo) => (
