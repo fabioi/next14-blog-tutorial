2983e57c7ccbd2ed0e853c29d1a084d698bfe9fc
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-09-11 11:33:14 +0200

    improve style

--- src/app/about/projects/components/project-list.js
+++ src/app/about/projects/components/project-list.js
@@ -5,12 +5,12 @@ export default async function ProjectList() {
       const repos = await response.json()
 
       return (
-            <ul className="space-y-4">
+            <ul className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   {repos.map((repo) => (
                         <li key={repo.id} className="mb-4">
-                              <Card className="font-mono">
+                              <Card className="font-mono h-full">
                                     <div className="flex justify-between items-center mb-4">
-                                          <div>{repo.title}</div>
+                                          <div className="font-semibold">{repo.title}</div>
                                           <div className="flex items-center gap-1">
                                                 <span>⭐</span>{repo.stargazers_count}
                                           </div>
