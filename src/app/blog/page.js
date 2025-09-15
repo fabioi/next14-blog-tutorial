358212e151963bed146b88f0815372e7f7f0f0ee
Fabio Ingrasciotta <fabio.ingrasciotta@gmail.com>
2025-09-15 20:09:23 +0200

    Refactor BlogPostPage to dynamically load and display posts from the content directory; update navigation to include a link to the Blog page

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,24 +1,25 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
+import fs from 'fs'
+import path from 'path'
 
-export async function generateMetadata() {
-      return {
-            title: "Blog",
-            description: "Blog",
-      }
-}
+export default async function BlogPostPage() {
+      const files = fs.readdirSync(path.join(process.cwd(), 'src/app/content')).filter(file => file.endsWith('.mdx'))
 
-export default function BlogPage() {
+      const posts = await Promise.all(files.map(async file => {
+            const content = await fs.readFileSync(path.join(process.cwd(), 'src/app/content', file), 'utf8')
+            return {
+                  frontmatter: content.split('---')[1],
+                  slug: file.replace('.mdx', '')
+            }
+      }))
+
       return (
             <>
-                  <article className="prose dark:prose-invert">
-                        <MDXRemote source={
-                              `# Hello world
-
-This is from server component`
-                        }
-
-                        />
-                  </article>
+                  <h1>Recent posts</h1>
+                  <ul>
+                        {posts.map(post => (
+                              <li key={post.slug}>{post.frontmatter.title}</li>
+                        ))}
+                  </ul>
             </>
       )
 }
